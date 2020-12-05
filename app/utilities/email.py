8cb329417531510d97cf9135d08e2ab016e5638d
Damilare <blankgodd33@gmail.com>
2020-12-05 22:46:42 +0100

    progress

--- app/utilities/email.py
+++ app/utilities/email.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 # Author:   @AgbaD | @Agba_dr3
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from flask import current_app, render_template
+import smtplib
+import ssl
+
+
+def send_mail(subject, template, to, **kwargs):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = current_app.config['MAIL_PREFIX'] + subject
+    message["From"] = current_app.config['TMM_SUPPORT']
+    message["To"] = to
+
+    text = render_template(template + ".txt", **kwargs)
+    html = render_template(template + ".html", **kwargs)
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("mail.privateemail.com", 465, context=context) as server:
+        server.login(current_app.config['TMM_SUPPORT'], current_app.config['TMM_SUPPORT_PASSWORD'])
+        print("Sending email to {}".format(to))
+        server.sendmail(
+            current_app.config['TMM_SUPPORT'], to, message.as_string()
+        )
+        print("Mail sent to {} successfully".format(to))
+        print()
--- config.py
+++ config.py
@@ -9,9 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "7+/1@1m[[fvk$k@m??70[4o?aw/k4+sjhxj1%hxw[9ky2u2"
     TMM_ADMIN = os.environ.get('TMM_ADMIN') or "admin@themiddlemaan.com"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     TMM_ADMIN_PASSWORD = os.environ.get('TMM_ADMIN_PASSWORD') or "fvk$k@m"
+    TMM_SUPPORT = os.environ.get('TMM_SUPPORT') or "support@themiddlemaan.com"
+    TMM_SUPPORT_PASSWORD = os.environ.get('TMM_SUPPORT_PASSWORD') or "o?aw/k"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_PREFIX = '[TheMiddleMaan]'
 
     @staticmethod
