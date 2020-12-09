6fe130c3b01269b0a150cb3ae61e35ff255c0734
Damilare <blankgodd33@gmail.com>
2020-12-09 19:13:19 +0100

    moves

--- app/main/mainapp.py
+++ app/main/mainapp.py
@@ -20,15 +20,32 @@ def index():
     if request.method == "POST":
         pass
     products = Product.query.all()
+
+    # new arrivals
     now = datetime.utcnow()
     prod_time = {}
     for product in products:
         # get time difference between now and when product was created
         pass
 
-    # get the best selling categories
-    
-    # return render_template('index.html')
+    # get the top categories
+    top_categories = []
+    qty = [v for v in product_categories.values()]
+    qty_sorted = sorted(qty)
+    i = 1
+    while len(top_categories) < 4:
+        for k, v in product_categories.items():
+            if v == qty_sorted[-i]:
+                top_categories.append(k)
+                if len(top_categories) >= 4:
+                    break
+        while qty_sorted[-i] == qty_sorted[-(i+1)]:
+            del(qty_sorted[-i])
+        i += 1
+
+    # best sellers
+
+    return render_template('index.html', top_categories=top_categories)
 
 
 @main.route("/get_all_products")
@@ -56,4 +73,4 @@ def get_product(product, short_des):
         if product.short_description == short_des:
             return render_template('product.html', product=product)
         else:   # should not happen
-            return redirect(url_for("internal_server_error"))
+            return redirect(url_for("main.internal_server_error"))
