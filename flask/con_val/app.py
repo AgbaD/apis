cc750dcd7e03b7a0cd36d8e99bdd31f1bf731706
Damilare Agba <damilareagba1@gmail.com>
2021-11-01 00:50:55 +0100

    adding db

--- flask/con_val/app.py
+++ flask/con_val/app.py
@@ -6,11 +6,50 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 CORS(app)
+
+# admin client
+a_client = FaunaClient(secret=os.getenv('FAUNA_SECRET'))
+
+result = a_client.query(q.create_key({
+	{"database": q.database("contact_validator_app"), "role": "client"}}))
+client_secret = result['secret']
+
+# server client
+s_client = FaunaClient(secret=os.getenv('FAUNA_SERVER_SECRET'))
+
+s_client.query(q.create_collection({"name": "users"}))
+s_client.query(q.create_index(
+	{
+		"name": "users_by_username",
+		"source": q.collection("users"),
+		"permissions": {"read": "public"},
+		"terms": [{"field": ["data", "username"]}],
+		"unique": True
+	}
+))
+
+
+s_client.query(q.create_collection({"name": "Contacts"}))
+s_client.query(q.create_index(
+	{
+		"name": "contacts_by_id",
+		"source": q.collection("Contacts"),
+		"terms": [{"field": ["username", "contact", "id"]}]
+	}
+))
+
+
+
 
 @app.route("/register", methods=['POST'])
 def register():
